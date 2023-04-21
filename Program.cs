8323effcb9539dac2ff1472bbbabc490cde5ac04
serkov <combinatexy@gmsil.com>
2023-04-22 04:08:06 +0700

    основа

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿class Program 
+{
+   static void Main(string[] args) 
+   {
+      double fValue, secValue;
+      string operation;
+
+      Console.WriteLine("Введите число 1");
+      fValue = double.Parse(Console.ReadLine());
+      Console.WriteLine("Введите число 2");
+      secValue = double.Parse(Console.ReadLine());
+      operation = Console.ReadLine();
+
+      switch (operation) 
+      {
+        
+        default:
+        Console.WriteLine("Ошибка");
+        break;
+      }
+   }
+}
