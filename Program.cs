ad5fc708092676c738bdcb4bdface6b3c4991ae4
serkov <combinatexy@gmsil.com>
2023-04-22 04:46:05 +0700

    функция суммы сделана

--- Program.cs
+++ Program.cs
@@ -9,10 +9,14 @@
       fValue = double.Parse(Console.ReadLine());
       Console.WriteLine("Введите число 2");
       secValue = double.Parse(Console.ReadLine());
+      Console.WriteLine("выберите операцию");
       operation = Console.ReadLine();
 
       switch (operation) 
       {
+        case "+":
+        Console.WriteLine(fValue + secValue);
+        break;
         
         default:
         Console.WriteLine("Ошибка");
